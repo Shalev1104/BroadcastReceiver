d90c0bca434b14dda4235dff11dca9c55a33eb0a
Shalev1104 <Shalev0125@gmail.com>
2020-02-26 13:28:25 +0200

    Broadcast Reciever

---
+++ Broadcast Receiver/Broadcast Receiver/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace Broadcast_Receiver
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private SampleReciever reciever;
+        private Button broadcast;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            reciever = new SampleReciever();
+            SetViews();
+        }
+        public void SetViews()
+        {
+            broadcast = FindViewById<Button>(Resource.Id.btnBroadcastReciever);
+            broadcast.Click += Broadcast_Click;
+        }
+
+        private void Broadcast_Click(object sender, System.EventArgs e)
+        {
+            Intent message = new Intent("com.xamarin.example.TEST");
+            message.PutExtra("key", "value");
+            SendBroadcast(message);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            RegisterReceiver(reciever, new Android.Content.IntentFilter("com.xamarin.example.TEST"));
+        }
+        protected override void OnPause()
+        {
+            UnregisterReceiver(reciever);
+            base.OnPause();
+        }
+    }
+}
---
+++ Broadcast Receiver/Broadcast Receiver/SampleReciever.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Broadcast_Receiver
+{
+    [BroadcastReceiver(Enabled = true, Exported = false)]
+    public class SampleReciever : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            {
+                string value = intent.GetStringExtra("key");
+
+                string toastStr = string.Format("Received intent! – Intent Action:{ 0}, Value ={ 1}",intent.Action, value);
+
+                Toast.MakeText(context, toastStr,
+                            ToastLength.Long).Show();
+            }
+        }
+    }
+}
